5fba4d037f3def836d1894533f8a0954c44e6cb9
Jasmina Shonazmi <73155297+Jas-mine19@users.noreply.github.com>
2022-12-11 19:26:49 +0500

    add app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded",( )=>{
+    const grid = document.querySelector('.grid');
+    let squares = Array.from(document.querySelectorAll('.grid div'));
+    const scoreDisplay = document.querySelector('#score');
+    const startBtn = dociment
+
+
+const zTetromino =[
+    [0,width,width+1,width*2+1],
+    [width+1,width+2,width*2,width*2+1],
+    [0,width,width+1,width*2+1],
+    [width+1,width+2,width*2,width*2+1]
+];
+
+const oTeromine= [
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1]
+];
+
+})
